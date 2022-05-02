3580a422126e8a0918a98ba36c073f9ad1b495bf
root <yluo3421@gmail.com>
2022-05-02 12:05:15 -0400

    commit tree 9d77b16861bb8a68e5ec9f8bdd7b50306b5ff115

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    changeText()
+  });
+  function changeText() {
+    document.getElementById( "text" ).innerHTML = "This is really cool!";
+  }
